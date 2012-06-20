4a464e23b9d21a703accac2c8654d14e7315c033
ctorres <ctorres@onlifehealth.com>
2012-06-20 14:00:36 -0500

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'b491073f13e185f8a17fec26d9e679e548f4d8738a02a0725a52c52318453523ee2dde146aa40ebcbe41fd4705421e77bb692132e71e127cc2d529af7ae8c644'
