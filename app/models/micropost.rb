f7ecb0dc1adc83342aedf947d1e6d919a380decb
ctorres <ctorres@onlifehealth.com>
2012-07-02 11:24:18 -0500

    Finish the Demoap

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,8 @@
 class Micropost < ActiveRecord::Base
 
   belongs_to :user
+  attr_accessible :contents, :user_id
+
   validates :contents, :length => { :maximum => 140}
 
-  attr_accessible :contents, :user_id
 end
